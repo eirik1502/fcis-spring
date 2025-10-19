48ab63710804c78c7190ef2423812192132f5efb
Eirik HS <eirik.skjaerseth@gmail.com>
2025-10-19 13:20:21 +0200

    gjør effekt utfører til et grensesnitt

--- src/main/kotlin/com/example/demo/skall/EffektUtførerConfig.kt
+++ src/main/kotlin/com/example/demo/skall/EffektUtførerConfig.kt
@@ -3,6 +3,7 @@ package com.example.demo.skall
 import com.example.demo.kjerne.LagreSykmelding
 import com.example.demo.kjerne.SlettSykmelding
 import com.example.demo.skall.rammeverk.EffektUtfører
+import com.example.demo.skall.rammeverk.lagEffektUtfører
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Configuration
 class EffektUtførerConfig {
     @Bean
     fun slettSykmeldingEffektUtfører(sykmeldingRepository: SykmeldingRepository) =
-        EffektUtfører(SlettSykmelding::class) { effekt ->
+        lagEffektUtfører(SlettSykmelding::class) { effekt ->
             sykmeldingRepository.findBySykmeldingId(effekt.sykmeldingId)?.let {
                 sykmeldingRepository.delete(it)
             }
@@ -18,7 +19,7 @@ class EffektUtførerConfig {
 
     @Bean
     fun lagreSykmeldingEffektUtfører(sykmeldingRepository: SykmeldingRepository) =
-        EffektUtfører(LagreSykmelding::class) { effekt ->
+        lagEffektUtfører(LagreSykmelding::class) { effekt ->
             sykmeldingRepository.save(effekt.sykmelding)
         }
 }
--- src/main/kotlin/com/example/demo/skall/rammeverk/types.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demo.skall.rammeverk
-
-import com.example.demo.kjerne.Effekt
-import kotlin.reflect.KClass
-
-class EffektUtfører<E : Effekt>(
-    val effektType: KClass<E>,
-    val utfør: (E) -> Unit,
-)
